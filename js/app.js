00f675de802585d6db547ef85176616d8ec533aa
Nicolas Quinn <nclsquinn@gmail.com>
2022-09-23 12:32:16 -0300

    Created an alert when some of the inputs is empty

--- js/app.js
+++ js/app.js
@@ -13,13 +13,28 @@ function guardarCliente () {
     const mesa = document.querySelector('#mesa').value;
     const hora = document.querySelector('#hora').value;
 
-
     const campoVacio = [ mesa, hora ].some( campo => campo === '' );
+
     if (campoVacio) {
-        console.log("Al menos 1 campo está vacío")
-    } else {
-        console.log("Ambos campos están llenos.")
+        // Revisa si existe una alerta
+        const alertaExiste = document.querySelector('div.invalid-feedback');
+        if (!alertaExiste) {
+
+            // Creo e inserto
+            const alerta = document.createElement('DIV');
+            alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+            alerta.textContent = 'Todos los campos son obligatorios.'
+            document.querySelector('.modal-dialog form').appendChild(alerta);
+
+            setTimeout(() => {
+                alerta.remove();
+            }, 3500);
+        }
+
+        return; // corto la función.
     }
+
+    console.log("Todo lleno.")
 
 }
 
